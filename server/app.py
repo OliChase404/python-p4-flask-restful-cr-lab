38b945793fde1afb4cec16b7b643139df9ab6a6e
Oliver <olichase91@gmail.com>
2023-04-30 16:39:26 -0700

    Solved Lab

--- server/app.py
+++ server/app.py
@@ -16,12 +16,52 @@ db.init_app(app)
 
 api = Api(app)
 
+class Home(Resource):
+    def get(self):
+        return make_response(jsonify({'message': 'Welcome to the Plants API'}), 200)
+    
+api.add_resource(Home, '/')
+
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return jsonify([plant.to_dict() for plant in plants])
+    
+    def post(self):
+        plant = Plant(**request.get_json())
+        db.session.add(plant)
+        db.session.commit()
+        return jsonify(plant.to_dict())
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, plant_id):
+        plant = Plant.query.get(plant_id)
+        return jsonify(plant.to_dict())
+    
+    def put(self, plant_id):
+        plant = Plant.query.get(plant_id)
+        data = request.get_json()
+        plant.name = data['name']
+        plant.image = data['image']
+        plant.price = data['price']
+        db.session.commit()
+        return jsonify(plant.to_dict())
+    
+    def patch(self, plant_id):
+        plant = Plant.query.get(plant_id)
+        plant.update(**request.get_json())
+        db.session.commit()
+        return jsonify(plant.to_dict())
+    
+    def delete(self, plant_id):
+        plant = Plant.query.get(plant_id)
+        db.session.delete(plant)
+        db.session.commit()
+        return jsonify(plant.to_dict())
         
+api.add_resource(PlantByID, '/plants/<int:plant_id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
